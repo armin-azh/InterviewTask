e261a1a184471d101c1b96d501c6bb90bb94895e
Armin Azhdehnia <arminmk18@gmail.com>
2024-09-02 17:12:38 +0000

    add logger to Inference service

--- services/source/src/rpc.py
+++ services/source/src/rpc.py
@@ -1,6 +1,9 @@
 # This component is responsible to implement gRPC
+import logging
 from src.proto.detection_srv_pb2_grpc import DetectionService
 from src.proto import detection_srv_pb2 as d_pb
+
+logger = logging.getLogger('rpc')
 
 __all__ = [
     'DetectionInferenceService'
@@ -9,7 +12,9 @@ __all__ = [
 
 class DetectionInferenceService(DetectionService):
     async def DetectSingleImage(self, request: d_pb.DetectSingleImageRequest, context)->d_pb.DetectSingleImageResponse:
+        logger.info('detect single image is called')
         return d_pb.DetectSingleImageResponse()
     
     async def DetectImages(self, request: d_pb.DetectImagesReqeust, context)->d_pb.DetectImagesResponse:
+        logger.info('detect image list is called')
         return d_pb.DetectImagesResponse()
