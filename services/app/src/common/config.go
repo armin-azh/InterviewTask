0b76d7f893ca0ddd082694473d664ecd4539cba5
Armin Azhdehnia <arminmk18@gmail.com>
2024-09-02 10:45:57 +0330

    add configs

---
+++ services/app/src/common/config.go
@@ -0,0 +1,36 @@
+package common
+
+import "github.com/spf13/viper"
+
+
+
+type Config struct {
+	Debug        bool   `mapstructure:"DEBUG"`
+	Port         int    `mapstructure:"PORT"`
+	Host         string `mapstructure:"HOST"`
+	DatabaseUrl  string `mapstructure:"DATABASE_URL"`
+}
+
+
+func LoadConfig(path string)(*Config, error){
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	// Override configs when environment variables occure
+	viper.AutomaticEnv()
+
+	err:=viper.ReadInConfig()
+	if err != nil{
+		return nil, err
+	}
+
+	var config Config
+
+	err = viper.Unmarshal(&config)
+
+	if err!=nil{
+		return nil, err
+	}
+	return &config, nil
+}
--- services/app/src/server.go
+++ services/app/src/server.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
+	"interview.com/app/src/common"
 )
 
 
@@ -10,11 +14,32 @@ import (
 func main() {
 	log.Info("Starting App Gateway service")
 
+	cfgPath:=os.Getenv("APP_CONFIG")
+	if cfgPath==""{
+		cfgPath = "."
+	}
+
+	cfgPathInfo, err:=os.Stat(cfgPath)
+	if err!=nil{
+		log.Error("Cannot find config file", err)
+		return
+	}
+	if !cfgPathInfo.IsDir(){
+		log.Errorf("%s is not a directory", cfgPath)
+		return 
+	}
+	
+	config, err := common.LoadConfig(cfgPath)
+	if err!=nil{
+		log.Error(err)
+		return
+	}
+
 	app:=fiber.New(fiber.Config{
 		AppName: "App Gateway Service",
 	})
 
-	if err:= app.Listen(":8080");err!=nil{
+	if err:= app.Listen(fmt.Sprintf("%s:%d", config.Host,config.Port));err!=nil{
 		log.Fatal("Cannot start server", err)
 	}
 }
