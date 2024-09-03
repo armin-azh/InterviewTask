6c6cdd0f8ff86e575b1e033e1cafa3034c569aef
Armin Azhdehnia <arminmk18@gmail.com>
2024-09-03 14:20:20 +0330

    add tab selector

---
+++ services/frontend/src/components/Home/UploadVideo/FaceScan.tsx
@@ -0,0 +1,11 @@
+import Lottie from "lottie-react";
+
+// Assets
+import faceScan from '@/assets/face-scan.json';
+
+
+
+export default function FaceScan(){
+
+    return <Lottie animationData={faceScan} />
+}
--- services/frontend/src/components/Home/UploadVideo/index.tsx
+++ services/frontend/src/components/Home/UploadVideo/index.tsx
@@ -1,6 +1,23 @@
 
+// Component
+import FaceScan from "@/components/Home/UploadVideo/FaceScan";
 
 export default function  UploadVideo(){
 
-    return null
+    return <div className='flex flex-col justify-center w-full py-10 gap-6'>
+
+        <div className='flex w-full justify-center'>
+            <div className='flex flex-col justify-center w-32'>
+                <FaceScan/>
+            </div>
+        </div>
+        <div className='flex justify-center'>
+            <div className='flex flex-col justify-center pt-3 tracking-widest'>
+                <h1 className='font-medium text-xl capitalize text-blue-600'>Upload video</h1>
+                <p className='text-sm text-gray-600'>
+                    Upload a query video to search across enrolled faces
+                </p>
+            </div>
+        </div>
+    </div>
 }
